e230bcec4bbbad9598f66470715b9ca3e3b5ae9d
MaxInGaussian <maxingaussian@gmail.com>
2017-10-25 13:59:11 +0800

    update VAF

--- KEGG/training.py
+++ KEGG/training.py
@@ -39,7 +39,7 @@ def load_data(n_folds):
     import pandas as pd
     data = pd.DataFrame.from_csv(path=DATA_PATH, header=None, index_col=0)
     data = data.as_matrix().astype(np.float32)
-    X, y = data[:, :-1], data[:, -1]
+    X, y = np.hstack((data[:, :-2], data[:, -1][: None])), data[:, -2]
     y = y[:, None]
     n_data = y.shape[0]
     n_partition = n_data//n_folds
