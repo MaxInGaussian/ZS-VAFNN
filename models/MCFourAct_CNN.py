d2d7019c61894487658db1aae522646fefb2293c
MaxInGaussian <maxingaussian@gmail.com>
2017-11-10 21:35:42 +0800

    update expts

--- models/MCFourAct_CNN.py
+++ models/MCFourAct_CNN.py
@@ -20,24 +20,37 @@ from __future__ import division
 import numpy as np
 import zhusuan as zs
 import tensorflow as tf
+import tensorflow.contrib.layers as layers
 
 
 def get_w_names(drop_rate, net_sizes):
-    return ['w'+str(i) for i in range(len(net_sizes)-1)]
+    w_names = []
+    return w_names
 
 @zs.reuse('model')
 def p_Y_Xw(observed, X, drop_rate, n_basis, net_sizes, n_samples, task):
     with zs.BayesianNet(observed=observed) as model:
         f = tf.expand_dims(tf.tile(tf.expand_dims(X, 0), [n_samples, 1, 1]), 2)
         for i in range(len(net_sizes)-1):
-            w_mu = tf.zeros([1, net_sizes[i]+1, net_sizes[i+1]])
-            w = zs.Normal('w'+str(i), w_mu, std=1.,
-                          n_samples=n_samples, group_ndims=2)
-            w = tf.tile(w, [1, tf.shape(X)[0], 1, 1])
-            f = tf.concat([f, tf.ones([n_samples, tf.shape(X)[0], 1, 1])], 3)
-            f = tf.matmul(f, w) / tf.sqrt(net_sizes[i]+1.)
+            conv = tf.layers.conv2d(
+                inputs=f,
+                filters=32,
+                kernel_size=[5, 5],
+                padding="same",
+                activation=None)
             if(i < len(net_sizes)-2):
-                f = tf.nn.relu(f)
+                omega_mean = tf.get_variable('omega_mean'+str(i),
+                    shape=[1, net_sizes[i+1], n_basis],
+                    initializer=tf.constant_initializer(0.))
+                omega_logstd = tf.get_variable('omega_logstd'+str(i),
+                    shape=[1, net_sizes[i+1], n_basis],
+                    initializer=tf.constant_initializer(0.))
+                omega_i = zs.Normal('omega'+str(i), omega_mean,
+                    logstd=omega_logstd, n_samples=n_samples, group_ndims=2)
+                omega_i = tf.tile(omega_i, [1, tf.shape(X)[0], 1, 1])
+                f = tf.matmul(f, omega_i)/tf.sqrt(net_sizes[i+1]*1.)
+                f = tf.concat([tf.cos(f),tf.sin(f)], 3)
+            pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 2], strides=2)
         f = tf.squeeze(f, [2])
         if(task == "regression"):
             y_logstd = tf.get_variable('y_logstd', shape=[],
@@ -46,17 +59,3 @@ def p_Y_Xw(observed, X, drop_rate, n_basis, net_sizes, n_samples, task):
         elif(task == "classification"):
             y = zs.OnehotCategorical('y', f)
     return model, f, None
-
-@zs.reuse('variational')
-def var_q_w(n_basis, net_sizes, n_samples):
-    with zs.BayesianNet() as variational:
-        for i in range(len(net_sizes)-1):
-            w_mean = tf.get_variable('w_mean'+str(i),
-                shape=[1, net_sizes[i]+1, net_sizes[i+1]],
-                initializer=tf.constant_initializer(0.))
-            w_logstd = tf.get_variable('w_logstd'+str(i),
-                shape=[1, net_sizes[i]+1, net_sizes[i+1]],
-                initializer=tf.constant_initializer(0.))
-            zs.Normal('w' + str(i), w_mean, logstd=w_logstd,
-                      n_samples=n_samples, group_ndims=2)
-    return variational
